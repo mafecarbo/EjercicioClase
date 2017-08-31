24ab0f1ac273c67afb29ff4889213b79516a9773
myd.abogados <myd.abogados@DIANA-PC>
2017-08-30 22:43:43 -0500

    holaaa

--- src/ejercicoclase2/EjercicoClase2.java
+++ src/ejercicoclase2/EjercicoClase2.java
@@ -16,6 +16,14 @@ public class EjercicoClase2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Tienda t =new Tienda("Donde Lucho", 33);
+        Producto[] p= new Producto[4];
+        p[0]= new Producto("Leche", 20, 0.04, 5, "SuperMercado", 3000);
+        p[1]= new Producto("Huevos", 30, 0.04, 10, "SuperMercado", 300);
+        p[2]= new Producto("Advil", 40, 0.12, 15, "Drogeria", 1000);
+        p[3]= new Producto("Lapiz", 30, 0.16, 10, "Papeleria", 700);
+        t.setProductos(p);
+        
     }
     
 }
